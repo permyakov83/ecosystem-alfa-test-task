003d315e111daaf606f6034c081c598a10219c4f
permyakov83 <permyakov83@gmail.com>
2024-08-27 14:55:56 +0500

    get store

--- src/features/CardList/CardList.tsx
+++ src/features/CardList/CardList.tsx
@@ -1,11 +1,19 @@
-import { store } from "../../app/store"
+import { useAppDispatch } from "../../app/hooks"
+import { type AppDispatch, store } from "../../app/store"
 import Card from "../../components/Card/Card"
-import useGetCardsData from "../../hooks/useGetCardsData"
+import { useGetFactsQuery } from "./factsApiSlice"
+import { useGetImagesQuery } from "./imagesApiSlice"
+import { type TCardsData } from "../../hooks/useGetCardsData"
+import { DataMerging } from "../../utils/dataMerging"
+import { cardsActions } from "./cardsSlice"
 
 const CardList = () => {
-  const cardData = useGetCardsData()
+  const dispatch = useAppDispatch<AppDispatch>()
 
-  if (cardData.isError) {
+  const facts = useGetFactsQuery(9)
+  const images = useGetImagesQuery(9)
+
+  if (facts.isError || images.isError) {
     return (
       <div>
         <h1>Что-то пошло не так :(</h1>
@@ -13,8 +21,7 @@ const CardList = () => {
     )
   }
 
-  if (cardData.isLoading) {
-    console.log("Загружаем информацию про кошек...")
+  if (facts.isLoading && images.isLoading) {
     return (
       <div>
         <h1>Загружаем информацию про кошек...</h1>
@@ -22,19 +29,33 @@ const CardList = () => {
     )
   }
 
-  if (cardData.isSuccess) {
-    console.log("Загрузили информацию про кошек...")
-    const cards = store.getState().cardsData.data
+  if (
+    facts.isSuccess &&
+    images.isSuccess &&
+    facts.data !== undefined &&
+    images.data !== undefined
+  ) {
+    const cards: TCardsData = {
+      data: DataMerging({
+        facts: facts.data,
+        images: images.data,
+      }),
+    }
+
+    dispatch(cardsActions.addCardsData(cards.data))
+
+    const cardsData = store.getState().cardsData.data
+
     return (
       <>
-        {cards.length === 0 ? (
+        {cardsData.length === 0 ? (
           <div className="p-10">
             Что то пошло не так и данные не загрузились...{" "}
             <span className="text-xl font-bold text-red-500">:(</span>
           </div>
         ) : (
           <ul className="flex flex-wrap justify-center gap-6 rounded-lg p-8">
-            {cards.map(item => (
+            {cardsData.map(item => (
               <li key={item.id}>
                 <Card
                   id={item.id}
